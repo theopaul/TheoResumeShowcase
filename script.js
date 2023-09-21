9cee9e996eff39abf2367a2a2054077dfb15db9d
theopaul <theops2@gmail.com>
2023-09-21 08:39:18 +0200

    Changes
    Removed the nested styles inside body since CSS does not support nesting in the same way as LESS or SASS.
    Moved the styles for anchor tags (a) out of the body scope to make them global.
    Added comments for better readability and understanding of what each style is doing.

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-function toggleMenu() {
-    const sections = document.querySelectorAll("section");
-    sections.forEach((section) => {
-      if (section.style.display === "none" || section.style.display === "") {
-        section.style.display = "block";
-      } else {
-        section.style.display = "none";
-      }
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("main > section");
+
+  sections.forEach((section) => {
+    const header = section.querySelector("h2");
+    const content = Array.from(section.children).filter(
+      (child) => child !== header
+    );
+
+    content.forEach((element) => {
+      element.style.display = "none";
     });
-  }
-  
+
+    header.addEventListener("click", () => {
+      content.forEach((element) => {
+        if (element.style.display === "none") {
+          element.style.display = "block";
+        } else {
+          element.style.display = "none";
+        }
+      });
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Existing code for other sections
+  const skillCategories = document.querySelectorAll("#skills .skill-category");
+
+  skillCategories.forEach((category) => {
+    const header = category.querySelector("h3");
+    const content = Array.from(category.children).filter(
+      (child) => child !== header
+    );
+
+    content.forEach((element) => {
+      element.style.display = "none";
+    });
+
+    header.addEventListener("click", () => {
+      content.forEach((element) => {
+        if (element.style.display === "none") {
+          element.style.display = "block";
+        } else {
+          element.style.display = "none";
+        }
+      });
+    });
+  });
+});
